ad43d23b0772d72b1c39b7364278162ba7a63d1c
usaikoo <saiko@tentaq.com>
2024-05-21 20:06:57 -0400

    add the example usage

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+
+## 0.0.3
+
+* add the example usage 
+
 ## 0.0.2
 
 * Add the bad words list in english and spanish
--- example/main.dart
+++ example/main.dart
@@ -1,20 +1,3 @@
-# Profanity Filter
-
-Profanity Filter is a Dart package that helps you detect and censor profanity in text strings. It provides a simple API to check if a given text contains profanity and to censor profanity by replacing it with asterisks.
-
-## Installation
-
-To use Profanity Filter in your Dart project, add it to your `pubspec.yaml` file:
-
-```yaml
-dependencies:
-  flutter_profanity_filter: ^0.0.2
-
-
-```
-
-```dart
-
 // example/main.dart
 import 'package:flutter_profanity_filter/profanity_filter.dart';
 
@@ -29,5 +12,3 @@ void main() {
   print(filter.censor('This is a fuck')); // 'This is a *******'
   print(filter.censor('This is clean')); // 'This is clean'
 }
-
-```
